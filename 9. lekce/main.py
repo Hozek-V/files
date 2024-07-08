ad9c793215573bc3a6fdab975b61633748175194
Hozek-V <janvystup@seznam.cz>
2024-07-08 19:08:30 +0200

    task 1

--- 9. lekce/main.py
+++ 9. lekce/main.py
@@ -1,6 +1,6 @@
 
 
-
+"""
 file = open("test.txt", "w")
 
 file.write("Ahoj světe! Jak se máš?")
@@ -11,3 +11,14 @@ file = open("test.txt", "r")
 data = file.read()
 file.close()
 print(data)
+"""
+#Task 1
+source_file = open("task_1_help.txt", "r")
+source_data = source_file.read()
+source_file.close()
+
+new_file_1 = open("new_file_1.txt", "w")
+new_file_1.write(source_data)
+new_file_1.close()
+
+#Task
